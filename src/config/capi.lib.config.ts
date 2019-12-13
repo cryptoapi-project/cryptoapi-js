560ed961f59ef3c7f4f7e0e854dd8af98cf2f53d
Anastasia Rudenko <a.rudenko@pixelplex.io>
2019-12-13 22:05:16 +0300

    up version

--- src/config/capi.lib.config.ts
+++ src/config/capi.lib.config.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_TIMEOUT_REQUEST = 30000;
 
-export const ETH_BASE_HTTP_URL = `https://697-crypto-api.pixelplex-test.com/api/v1`;
+export const ETH_BASE_HTTP_URL = 'https://697-crypto-api.pixelplex-test.by/api/v1';
 export const ETH_BASE_WS_URL = 'wss://697-crypto-api.pixelplex-test.by/ws/eth';
 
 export const ETH_BASE_WS_RECONNECT = false;
