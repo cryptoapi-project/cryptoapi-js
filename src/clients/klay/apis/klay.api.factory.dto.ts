2a42759d88a935edba67fc0998d05268a6e1c68d
Anton Zimnitski <a.zimnitski@pixelplex.io>
2020-01-31 17:55:38 +0300

    fix dtos and factory

--- src/clients/klay/apis/klay.api.factory.dto.ts
+++ src/clients/klay/apis/klay.api.factory.dto.ts
@@ -2,6 +2,7 @@ import { injectable } from 'inversify';
 
 import { KlayAddressBalance } from '@src/dtos/klay/klay.address.balance';
 import { KlayAddressInfo } from '@src/dtos/klay/klay.address.info';
+import { KlayBlockInfo, KlayBlocksResponse } from '@src/dtos/klay/klay.block.dtos';
 import { EstimateGasResponse } from '@src/dtos/klay/klay.estimate.gas.dto';
 import { KlayNetworkInfo } from '@src/dtos/klay/klay.network.info';
 import { IKlayApiFactoryDto } from '@src/interfaces/clients/klay/apis/klay.api.factory.dto.interface';
@@ -23,5 +24,13 @@ export class KlayApiFactoryDto implements IKlayApiFactoryDto {
 
 	getAddressInfo(data: any): KlayAddressInfo {
 		return new KlayAddressInfo(data);
+	}
+
+	getBlock(data: any): KlayBlockInfo {
+		return new KlayBlockInfo(data);
+	}
+
+	getBlocks(data: any): KlayBlocksResponse {
+		return new KlayBlocksResponse(data);
 	}
 }
--- src/dtos/klay/klay.block.dtos.ts
+++ src/dtos/klay/klay.block.dtos.ts
@@ -1,88 +1,82 @@
+
 export class KlayBlockInfo {
+	transactions: string[];
 	size: number;
-	difficulty: number;
-	total_difficulty: number;
-	uncles: string[];
+	block_score: string;
+	total_block_score: string;
 	number: number;
 	hash: string;
 	parent_hash: string;
-	nonce: string;
-	sha3_uncles: string;
+	reward: string;
+	governance_data: string;
+	vote_data: string;
+	timestamp_fos: string;
 	logs_bloom: string;
 	transaction_root: string;
 	state_root: string;
 	receipts_root: string;
-	miner: string;
-	mix_hash: string;
 	extra_data: string;
-	gas_limit: number;
 	gas_used: number;
 	utc: string;
-	transactions: string[];
 
 	constructor({
+		transactions,
 		size,
-		difficulty,
-		total_difficulty,
-		uncles,
+		block_score,
+		total_block_score,
 		number,
 		hash,
 		parent_hash,
-		nonce,
-		sha3_uncles,
+		reward,
+		governance_data,
+		vote_data,
+		timestamp_fos,
 		logs_bloom,
 		transaction_root,
 		state_root,
 		receipts_root,
-		miner,
-		mix_hash,
 		extra_data,
-		gas_limit,
 		gas_used,
 		utc,
-		transactions,
 	}: {
-		size: number,
-		difficulty: number,
-		total_difficulty: number,
-		uncles: string[],
-		number: number,
-		hash: string,
-		parent_hash: string,
-		nonce: string,
-		sha3_uncles: string,
-		logs_bloom: string,
-		transaction_root: string,
-		state_root: string,
-		receipts_root: string,
-		miner: string,
-		mix_hash: string,
-		extra_data: string,
-		gas_limit: number,
-		gas_used: number,
-		utc: string,
-		transactions: string[],
+		transactions: string[];
+		size: number;
+		block_score: string;
+		total_block_score: string;
+		number: number;
+		hash: string;
+		parent_hash: string;
+		reward: string;
+		governance_data: string;
+		vote_data: string;
+		timestamp_fos: string;
+		logs_bloom: string;
+		transaction_root: string;
+		state_root: string;
+		receipts_root: string;
+		extra_data: string;
+		gas_used: number;
+		utc: string;
+
 	}) {
+		this.transactions = transactions;
 		this.size = size;
-		this.difficulty = difficulty;
-		this.total_difficulty = total_difficulty;
-		this.uncles = uncles;
+		this.block_score = block_score;
+		this.total_block_score = total_block_score;
 		this.number = number;
 		this.hash = hash;
 		this.parent_hash = parent_hash;
-		this.nonce = nonce;
-		this.sha3_uncles = sha3_uncles;
+		this.reward = reward;
+		this.governance_data = governance_data;
+		this.vote_data = vote_data;
+		this.timestamp_fos = timestamp_fos;
 		this.logs_bloom = logs_bloom;
 		this.transaction_root = transaction_root;
 		this.state_root = state_root;
 		this.receipts_root = receipts_root;
-		this.miner = miner;
-		this.mix_hash = mix_hash;
 		this.extra_data = extra_data;
-		this.gas_limit = gas_limit;
 		this.gas_used = gas_used;
 		this.utc = utc;
-		this.transactions = transactions;
 	}
 }
 
